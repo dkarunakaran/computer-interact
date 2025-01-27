ffd2fdafebd7b774fefd8ad325cc94209dbdf3b0
Beastan <dhanoop.karunakaran@gmail.com>
2025-01-27 12:38:31 +1100

    changes

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # Broswe-bot
-<!--
-<picture>
-  <source media="(prefers-color-scheme: dark)" srcset="./static/browser-use-dark.png">
-  <source media="(prefers-color-scheme: light)" srcset="./static/browser-use.png">
-  <img alt="Shows a black Browser Use Logo in light color mode and a white one in dark color mode." src="./static/browser-use.png"  width="full">
-</picture>-->
-
-<br/>
 
 A library for automating web tasks
 
 To learn more about the library, check out the [documentation 📕]
 
-# Quick start
+# Instruction to start
 
 With pip:
 
--- src/browse-bot/supervisor.py
+++ src/browse-bot/supervisor.py
@@ -13,36 +13,39 @@ from agents.agent_state import AgentState
 from typing import Literal
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import HumanMessage, AIMessage
+from utils import logger_helper
 
 class SupervisorAgents:
     def __init__(self, openai_api_token=None):
         with open("config.yaml") as f:
-            self.cfg = yaml.load(f, Loader=yaml.FullLoader)
-        self.logger = self.logger_helper(self.cfg)
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+
+        # __ adding infront of the variable and method make them private
+        self.__logger = logger_helper(self.config)
         if not os.environ.get("OPENAI_API_KEY"):
             os.environ["OPENAI_API_KEY"] = openai_api_token
         llm = ChatOpenAI(model="gpt-4o-mini", temperature=0) 
-        self.gmail_agent = GmailAgent(cfg=self.cfg)
-        self.browser_agent = BrowserAgent(cfg=self.cfg)
-        workers_list = ['gmail_operation_agent', 'browser_operation_agent', 'none']
+        self.__gmail_agent = GmailAgent(cfg=self.config)
+        self.__browser_agent = BrowserAgent(cfg=self.config)
         system_prompt = (
             "You are a supervisor tasked with managing a conversation between the"
-            f" following workers: {','.join(workers_list)}. Given the following user request,"
+            f" following workers: 'gmail_operation_agent', 'browser_operation_agent', 'none'."
+            " Given the following user request,"
             " respond with the worker to act next. Each worker will perform a"
             " task and respond with their results and status. When finished,"
             " respond with FINISH."
             "If you can't find a suitable worker, then use 'none' worker."
         )
         prompt = ChatPromptTemplate.from_messages([("system", system_prompt),("human", "{input}")])
-        self.supervisor_chain = prompt | llm
+        self.__supervisor_chain = prompt | llm
         workflow = StateGraph(AgentState)
-        workflow.add_node("Supervisor", self.supervisor_node)
-        workflow.add_node("GmailAgent", self.gmail_agent_node)
-        workflow.add_node("BrowserAgent", self.browser_agent_node)
+        workflow.add_node("Supervisor", self.__supervisor_node)
+        workflow.add_node("GmailAgent", self.__gmail_agent_node)
+        workflow.add_node("BrowserAgent", self.__browser_agent_node)
         workflow.add_edge(START, "Supervisor")
         workflow.add_conditional_edges(
             "Supervisor",
-            self.router,
+            self.__router,
             {"gmail_operation_agent": "GmailAgent", "browser_operation_agent":"BrowserAgent", "__end__": END},
         )
         workflow.add_edge(
@@ -56,7 +59,7 @@ class SupervisorAgents:
         self.graph = workflow.compile()
 
     # This is the router
-    def router(self, state) -> Literal["gmail_operation_agent", "browser_operation_agent", "__end__"]:
+    def __router(self, state) -> Literal["gmail_operation_agent", "browser_operation_agent", "__end__"]:
             
         # Sleep to avoid hitting QPM limits
         last_result_text = state["message"][-1].content
@@ -77,72 +80,28 @@ class SupervisorAgents:
         
         return "Supervisor"
 
-    def supervisor_node(self, state: AgentState):
-        self.logger.info("Supervisor node started")
+    def __supervisor_node(self, state: AgentState):
+        self.__logger.info("Supervisor node started")
         message = state["message"]
-        result = self.supervisor_chain.invoke(message)
-        self.logger.debug(f"Supervisor result:{result}")
+        result = self.__supervisor_chain.invoke(message)
+        self.__logger.debug(f"Supervisor result:{result}")
         return {'message': [result], 'sender': ['supervisor']}
 
-    def gmail_agent_node(self, state: AgentState):
-        self.logger.info("Gmail agent node started")
+    def __gmail_agent_node(self, state: AgentState):
+        self.__logger.info("Gmail agent node started")
         context = state["message"][0]
         input = state["message"][-2]
-        result = self.gmail_agent.agent_executor.invoke({"chat_history":[], "agent_scratchpad":"", "context": self.gmail_agent.context+context,"input":input}, {"recursion_limit": self.cfg['GMAIL_AGENT']['recursion_limit']})
-        self.logger.debug(f"Gmail agent result:{result}")
+        result = self.__gmail_agent.agent_executor.invoke({"chat_history":[], "agent_scratchpad":"", "context": self.__gmail_agent.context+context,"input":input}, {"recursion_limit": self.config['GMAIL_AGENT']['recursion_limit']})
+        self.__logger.debug(f"Gmail agent result:{result}")
         return {'message': [result['output']], 'sender': ['gmail_agent']}
     
-    def browser_agent_node(self, state: AgentState):
-        self.logger.info("Browser agent node started")
+    def __browser_agent_node(self, state: AgentState):
+        self.__logger.info("Browser agent node started")
         context = state["message"][0]
         input = state["message"][-2]
-        result = self.browser_agent.agent_executor.invoke({"chat_history":[], "agent_scratchpad":"", "context": self.browser_agent.context+context,"input": input}, {"recursion_limit": self.cfg['BROWSER_AGENT']['recursion_limit']})
-        self.logger.debug(f"Browser agent result:{result}")
+        result = self.__browser_agent.agent_executor.invoke({"chat_history":[], "agent_scratchpad":"", "context": self.__browser_agent.context+context,"input": input}, {"recursion_limit": self.config['BROWSER_AGENT']['recursion_limit']})
+        self.__logger.debug(f"Browser agent result:{result}")
         return {'message': [result['output']], 'sender': ['browser_agent']}
-        
-
-    # Ref - https://medium.com/pythoneers/beyond-print-statements-elevating-debugging-with-python-logging-715b2ae36cd5
-    def logger_helper(self, cfg):
-
-        logger = logging.getLogger('my_logger')
-        logger.setLevel(logging.DEBUG)  # Capture all messages of debug or higher severity
-
-        ### File handler for errors
-        # Create a file handler that writes log messages to 'error.log'
-        file_handler = logging.FileHandler('error.log') 
-        # Set the logging level for this handler to ERROR, which means it will only handle messages of ERROR level or higher
-        file_handler.setLevel(logging.ERROR)  
-
-        ### Console handler for info and above
-        # Create a console handler that writes log messages to the console
-        console_handler = logging.StreamHandler()  
-        
-        if cfg['debug'] == True:
-            console_handler.setLevel(logging.DEBUG)  
-        else:
-            # Set the logging level for this handler to INFO, which means it will handle messages of INFO level or higher
-            console_handler.setLevel(logging.INFO)  
-
-        ### Set formats for handlers
-        # Define the format of log messages
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') 
-        # Apply the formatter to the file handler 
-        file_handler.setFormatter(formatter) 
-        # Apply the formatter to the console handler
-        console_handler.setFormatter(formatter)  
-
-        ### Add handlers to logger
-        # Add the file handler to the logger, so it will write ERROR level messages to 'error.log'
-        logger.addHandler(file_handler)  
-        # Add the console handler to the logger, so it will write INFO level messages to the console
-        logger.addHandler(console_handler)  
-
-        # Now when you log messages, they are directed based on their severity:
-        #logger.debug("This will print to console")
-        #logger.info("This will also print to console")
-        #logger.error("This will print to console and also save to error.log")
-
-        return logger
         
 
 if __name__ == "__main__":
@@ -171,7 +130,8 @@ if __name__ == "__main__":
     initial_state = AgentState()
     initial_state['message'] = [prompt1]
 
-    result = supervisor.graph.invoke(initial_state, {"recursion_limit": 10})
+    result = supervisor.graph.invoke(initial_state, {"recursion_limit": supervisor.config['SUPERVISOR']['recursion_limit']})
     print("-------------------------------------")
     print(f"Execution path: {result['sender']}")
+
    
