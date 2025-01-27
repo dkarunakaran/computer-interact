d340086b7ff8315338a2c6c97e8757bbc562c851
Beastan <dhanoop.karunakaran@gmail.com>
2025-01-28 00:34:04 +1100

    changes

--- run.bash
+++ run.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Run below step one by one in terminal
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
-docker build -t broswer_bot .
+docker build -t web_operator .
 
 # Get the current username
 username=$(whoami)
 if [ "$username" == "beastan" ]; then
     echo "We are running the docker container on $username's computer."
-    docker run --net host --gpus all -it -v /media/$username/projects/browse-bot:/app broswer_bot
+    docker run --net host --gpus all -it -v /media/$username/projects/web-operator:/app web_operator
 elif [ "$username" == "dhanoop" ]; then
     echo "We are running the docker container on $username's computer."
-    docker run --net host --gpus all -it -v /home/$username/Documents/projects/browse-bot:/app broswer_bot
+    docker run --net host --gpus all -it -v /home/$username/Documents/projects/web-operator:/app web_operator
 else
     echo "Wrong system to run this script."
 fi
